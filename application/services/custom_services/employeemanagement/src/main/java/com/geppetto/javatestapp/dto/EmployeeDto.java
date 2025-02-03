10cc481e431502b4ac13491e7975a3ef97df6277
Arun-YA <arun.ya@10decoders.in>
2025-02-03 11:14:43 +0530

    issue fixed

--- application/services/custom_services/employeemanagement/src/main/java/com/geppetto/javatestapp/dto/EmployeeDto.java
+++ application/services/custom_services/employeemanagement/src/main/java/com/geppetto/javatestapp/dto/EmployeeDto.java
@@ -1,6 +1,7 @@
 package com.geppetto.javatestapp.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Builder;
